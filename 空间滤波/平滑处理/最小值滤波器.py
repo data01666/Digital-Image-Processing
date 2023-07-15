9b2e10ca79ca8194e78a4eee702b5ff3a9bdffdd
data01666 <75118388+data01666@users.noreply.github.com>
2023-07-15 08:47:30 +0800

    Update 最小值滤波器.py

--- 空间滤波/平滑处理/最小值滤波器.py
+++ 空间滤波/平滑处理/最小值滤波器.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -24,4 +18,3 @@ axs[0].set_title('原图')
 axs[1].imshow(filtered_image, cmap='gray')
 axs[1].set_title('最小值滤波器')
 plt.show()
-
