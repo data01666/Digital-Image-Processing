1638bd104225a2f8298279286d5f12253dd2cd96
data01666 <75118388+data01666@users.noreply.github.com>
2023-07-16 09:24:13 +0800

    Update 巴特沃斯高通滤波器.py

--- 频率滤波/高通/巴特沃斯高通滤波器.py
+++ 频率滤波/高通/巴特沃斯高通滤波器.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -43,4 +37,3 @@ axs[0].set_title('原图')
 axs[1].imshow(filtered_image, cmap='gray')
 axs[1].set_title('巴特沃斯高通滤波器')
 plt.show()
-
